0bf595576ce67ee016e19c2e8865784f8a055a3b
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-13 22:50:15 +0100

    update

--- RazorViews/Controllers/HomeController.cs
+++ RazorViews/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using RazorViews.Models;
 namespace RazorViews.Controllers;
 
 public class HomeController : Controller
@@ -7,8 +7,16 @@ public class HomeController : Controller
     [Route("home")]
     public IActionResult Index()
     {
-        return View(); //Views/Home/Index.cshtml
-                       //return View("abc"); //abc.cshtml
-                       //return new ViewResult() { ViewName = "abc" };
+        ViewData["appTitle"] = "Asp.Net Core App";
+        List<Person> people = new List<Person>
+        {
+        new Person { Name = "John", DateOfBirth = DateTime.Parse("2027-05-05") },
+        new Person { Name = "Jane", DateOfBirth = DateTime.Parse("2027-05-05") }
+        };
+        ViewData["people"] = people;
+        return View(); 
+        //Views/Home/Index.cshtml
+        //return View("abc"); //abc.cshtml
+        //return new ViewResult() { ViewName = "abc" };
     }
 }
